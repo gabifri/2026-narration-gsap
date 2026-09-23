48be367bc4390df9778f85e7580111c62ed5fd96
Léo TORRAS <frigial.gaimeur@gmail.com>
2026-09-23 11:49:05 +0200

    :)

--- JS/main.js
+++ JS/main.js
@@ -1,7 +1,15 @@
 gsap.registerPlugin(ScrollTrigger,Observer,ScrollToPlugin,Draggable,MotionPathPlugin);
 
-gsap.to("#hero-content", {
-    x: "400px",
+gsap.to("#hero", {
+    scrollTrigger: {
+    trigger: '#hero-content',
+    start: 'top 60%',
+    end: 'top 20%',
+    scrub: 1,
+    markers: true,
+    id: "window2",
+    toggleActions: 'play none reverse reset',
+    },
     rotation: 360,
     duration: 2,
 })
